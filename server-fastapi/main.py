004a40d7ab1e0e7200652353b61d1f5a5126f570
makin <makin.ugm@gmail.com>
2026-03-26 21:35:06 +0700

    update uvcorn parameter

--- server-fastapi/main.py
+++ server-fastapi/main.py
@@ -120,4 +120,4 @@ if __name__ == "__main__":
         except ValueError:
             pass
     
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True, proxy_headers=True, forwarded_allow_ips="*")
